67dea72fc4bff4715e89a298272e3181fe9d0f33
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-03-20 09:55:59 +0100

    Fixed MetaVariable World

--- core/src/main/java/com/nisovin/magicspells/variables/meta/WorldVariable.java
+++ core/src/main/java/com/nisovin/magicspells/variables/meta/WorldVariable.java
@@ -4,13 +4,18 @@ import org.bukkit.entity.Player;
 import com.nisovin.magicspells.util.PlayerNameUtils;
 import com.nisovin.magicspells.variables.variabletypes.MetaVariable;
 
-public abstract class WorldVariable extends MetaVariable {
+public class WorldVariable extends MetaVariable {
 
 	@Override
 	public String getStringValue(String player) {
 		Player p = PlayerNameUtils.getPlayerExact(player);
 		if (p != null) return p.getLocation().getWorld().getName();
 		return null;
+	}
+
+	@Override
+	public double getValue(String player) {
+		return 0;
 	}
 
 }

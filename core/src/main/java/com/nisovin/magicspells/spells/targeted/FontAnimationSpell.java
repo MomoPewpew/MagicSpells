c23ff7353b27f8d4bad78292e6cb5389b1fd75c6
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-06-28 23:31:59 +0200

    Added fadeOut code

--- core/src/main/java/com/nisovin/magicspells/spells/targeted/FontAnimationSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/targeted/FontAnimationSpell.java
@@ -19,16 +19,23 @@ import com.nisovin.magicspells.spells.TargetedEntitySpell;
 public class FontAnimationSpell extends TargetedSpell implements TargetedEntitySpell {
 
 	private String fontName;
+
 	private int ticksPerFrame;
 	private int startFrame;
 	private int totalFrames;
 
+	private boolean fadeOut;
+
 	public FontAnimationSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
+
 		fontName = config.getString("font-name", null);
+
 		ticksPerFrame = config.getInt("ticks-per-frame", 1);
 		startFrame = Math.max(Math.min(config.getInt("start-frame", 0), 999), 0);
 		totalFrames = Math.max(Math.min(config.getInt("total-frames", 1), 1000 - startFrame), 1);
+
+		fadeOut = config.getBoolean("fade-out", false);
 	}
 
 	@Override
@@ -77,6 +84,13 @@ public class FontAnimationSpell extends TargetedSpell implements TargetedEntityS
 		while (iterator.hasNext()) {
 			MagicSpells.scheduleDelayedTask(() -> {
 				Component message = Component.text(iterator.next()).font(Key.key(fontName));
+				target.sendActionBar(message);
+			}, ticksPerFrame * n++);
+		}
+
+		if (!fadeOut) {
+			MagicSpells.scheduleDelayedTask(() -> {
+				Component message = Component.text(" ");
 				target.sendActionBar(message);
 			}, ticksPerFrame * n++);
 		}

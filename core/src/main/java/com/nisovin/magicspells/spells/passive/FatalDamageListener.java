7def6f7cc38c8f8a5e0202638b87e63b75a26129
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-12-28 09:41:54 +0100

    Added magicitem/damagecause var to fataldamage

--- core/src/main/java/com/nisovin/magicspells/spells/passive/FatalDamageListener.java
+++ core/src/main/java/com/nisovin/magicspells/spells/passive/FatalDamageListener.java
@@ -1,21 +1,58 @@
 package com.nisovin.magicspells.spells.passive;
 
 import org.bukkit.event.EventHandler;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.inventory.EntityEquipment;
 
 import com.nisovin.magicspells.util.OverridePriority;
+import com.nisovin.magicspells.util.magicitems.MagicItemData;
+import com.nisovin.magicspells.util.magicitems.MagicItems;
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.spells.passive.util.PassiveListener;
 
-// No trigger variable is used here
+// Optional trigger variable of a pipe separated list that can contain
+// damage causes or damaging magic items to accept
 public class FatalDamageListener extends PassiveListener {
+
+	private final EnumSet<DamageCause> damageCauses = EnumSet.noneOf(DamageCause.class);
+	private final Set<MagicItemData> items = new HashSet<>();
 
 	@Override
 	public void initialize(String var) {
+		if (var == null || var.isEmpty()) return;
 
+		String[] split = var.split("\\|");
+		for (String s : split) {
+			s = s.trim();
+
+			boolean isDamCause = false;
+			for (DamageCause c : DamageCause.values()) {
+				if (!s.equalsIgnoreCase(c.name())) continue;
+
+				damageCauses.add(c);
+				isDamCause = true;
+				break;
+			}
+			if (isDamCause) continue;
+
+			MagicItemData itemData = MagicItems.getMagicItemDataFromString(s);
+			if (itemData == null) {
+				MagicSpells.error("Invalid damage cause or magic item '" + s + "' in takedamage trigger on passive spell '" + passiveSpell.getInternalName() + "'");
+				continue;
+			}
+
+			items.add(itemData);
+		}
 	}
 
 	@OverridePriority
@@ -24,9 +61,20 @@ public class FatalDamageListener extends PassiveListener {
 		if (!(event.getEntity() instanceof LivingEntity caster)) return;
 		if (!isCancelStateOk(event.isCancelled())) return;
 		if (event.getFinalDamage() < caster.getHealth()) return;
-		if (!canTrigger(caster) || !hasSpell(caster)) return;
+		if (!hasSpell(caster) || !canTrigger(caster)) return;
+		if (!damageCauses.isEmpty() && !damageCauses.contains(event.getCause())) return;
 
 		LivingEntity attacker = getAttacker(event);
+
+		if (!items.isEmpty()) {
+			if (attacker == null) return;
+
+			EntityEquipment eq = attacker.getEquipment();
+			if (eq == null) return;
+
+			MagicItemData itemData = MagicItems.getMagicItemDataFromItemStack(eq.getItemInMainHand());
+			if (itemData == null || !contains(itemData)) return;
+		}
 
 		boolean casted = passiveSpell.activate(caster, attacker);
 		if (cancelDefaultAction(casted)) event.setCancelled(true);
@@ -41,4 +89,12 @@ public class FatalDamageListener extends PassiveListener {
 		}
 		return null;
 	}
+
+	private boolean contains(MagicItemData itemData) {
+		for (MagicItemData data : items) {
+			if (data.matches(itemData)) return true;
+		}
+		return false;
+	}
+
 }

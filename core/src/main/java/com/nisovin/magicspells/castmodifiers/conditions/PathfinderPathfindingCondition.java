7f2aa10c6921af8f70b9896915f60d3a5054c42d
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-06-29 10:03:35 +0200

    Downgraded pathfinding condition for 5.0.0

--- core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/PathfinderPathfindingCondition.java
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/PathfinderPathfindingCondition.java
@@ -1,8 +1,8 @@
 package com.nisovin.magicspells.castmodifiers.conditions;
 
-import de.cubbossa.pathfinder.misc.PathPlayer;
-import de.cubbossa.pathfinder.navigation.NavigationModule;
-import de.cubbossa.pathfinder.visualizer.VisualizerPath;
+import de.cubbossa.pathapi.navigation.NavigationHandler;
+import de.cubbossa.pathfinder.BukkitPathFinder;
+import de.cubbossa.pathfinder.module.BukkitNavigationHandler;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 
@@ -36,7 +36,7 @@ public class PathfinderPathfindingCondition extends Condition {
 
 	private boolean pathfinding(LivingEntity entity) {
 		if (entity instanceof Player player) {
-			@Nullable VisualizerPath<Object> path = NavigationModule.get().getActivePath(PathPlayer.wrap(player));
+			@Nullable NavigationHandler.SearchInfo<Player> path = BukkitNavigationHandler.getInstance().getActivePath(BukkitPathFinder.wrap(player));
 			return path != null;
 		}
 

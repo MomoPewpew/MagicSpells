053ba0b7bda45fff6168014f5b2bbb19a0736452
Zelereth <8508357+Zelereth@users.noreply.github.com>
2024-07-25 23:45:20 -0400

    Meta variables for attributes generate dynamically (MS4 Team).

--- core/src/main/java/com/nisovin/magicspells/variables/meta/AttributeBaseValueVariable.java
+++ core/src/main/java/com/nisovin/magicspells/variables/meta/AttributeBaseValueVariable.java
@@ -1,61 +1,41 @@
 package com.nisovin.magicspells.variables.meta;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 
-import com.nisovin.magicspells.util.PlayerNameUtils;
-import com.nisovin.magicspells.util.compat.CompatBasics;
 import com.nisovin.magicspells.variables.variabletypes.MetaVariable;
 
 public class AttributeBaseValueVariable extends MetaVariable {
 
-	private final String attributeName;
+	private final Attribute attribute;
 
-	private boolean safeHere = false;
-	private boolean safetyChecked = false;
-	
-	private Attribute targetAttribute = null;
-
-	public AttributeBaseValueVariable(String attributeName) {
+	public AttributeBaseValueVariable(Attribute attribute) {
 		super();
 
-		this.attributeName = attributeName;
-	}
-	
-	@Override
-	public double getValue(String player) {
-		if (!safetyChecked) {
-			safeHere = calculateIsSafeHere();
-			safetyChecked = true;
-		}
-		// Not usable here
-		if (!safeHere) return 0D;
-		
-		Player p = PlayerNameUtils.getPlayerExact(player);
-		if (p == null) return 0D;
-		
-		return p.getAttribute(targetAttribute).getBaseValue();
-	}
-	
-	@Override
-	public void set(String player, double amount) {
-		if (!safetyChecked) {
-			safeHere = calculateIsSafeHere();
-			safetyChecked = true;
-		}
-		
-		// Not usable here
-		if (!safeHere) return;
-		
-		Player p = PlayerNameUtils.getPlayerExact(player);
-		if (p == null) return;
-		
-		p.getAttribute(targetAttribute).setBaseValue(amount);
+		this.attribute = attribute;
 	}
 
-	private boolean calculateIsSafeHere() {
-		boolean safe = CompatBasics.runsWithoutError(() -> targetAttribute = Attribute.valueOf(attributeName));
-		return safe && targetAttribute != null;
+	@Override
+	public double getValue(String player) {
+		Player p = Bukkit.getPlayerExact(player);
+		if (p == null) return 0D;
+
+		AttributeInstance inst = p.getAttribute(attribute);
+		if (inst == null) return 0D;
+
+		return inst.getBaseValue();
 	}
-	
+
+	@Override
+	public void set(String player, double amount) {
+		Player p = Bukkit.getPlayerExact(player);
+		if (p == null) return;
+
+		AttributeInstance inst = p.getAttribute(attribute);
+		if (inst == null) return;
+
+		inst.setBaseValue(amount);
+	}
 }

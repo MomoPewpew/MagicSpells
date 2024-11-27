2f0953350a731857cf83b6e869cd7a8b725be728
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-11-27 06:57:33 +0100

    Fixed replace undo

--- core/src/main/java/com/nisovin/magicspells/spells/targeted/ReplaceSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/targeted/ReplaceSpell.java
@@ -292,7 +292,7 @@ public class ReplaceSpell extends TargetedSpell implements TargetedLocationSpell
 						// Break block.
 						if (resolveDurationPerBlock) replaceDuration = this.replaceDuration.get(caster, null, power, args);
 						if (replaceDuration > 0) {
-							AlteredBlockManager.Change change = new AlteredBlockManager.Change(internalName, block, block.getBlockData());
+							AlteredBlockManager.Change change = new AlteredBlockManager.Change(internalName, block, data);
 							MagicSpells.getAlteredBlockManager().add(change);
 
 							MagicSpells.scheduleDelayedTask(() -> {

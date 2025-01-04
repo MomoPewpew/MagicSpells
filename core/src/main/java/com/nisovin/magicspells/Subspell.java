99c05b67372734ad24f26c871bbf9eaa8b95a790
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2025-01-04 12:33:26 +0100

    Revert "On subcast with no target, target=caster"
    This reverts commit f3437a04ab29394d1e144fc7af80ed42d20329a0.

--- core/src/main/java/com/nisovin/magicspells/Subspell.java
+++ core/src/main/java/com/nisovin/magicspells/Subspell.java
@@ -236,7 +236,7 @@ public class Subspell {
 	}
 
 	public boolean subcast(SpellData data, boolean passTargeting, boolean useTargetForLocation) {
-		data = data.builder().target(data.target() == null ? data.caster() : data.target()).build();
+		data = data.builder().build();
 
 		if (invert) data.invert();
 

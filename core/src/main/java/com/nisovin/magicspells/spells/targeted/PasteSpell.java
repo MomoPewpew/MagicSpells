4c9f0d904f4db8828903c20c59733dbe539f443d
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2025-01-13 20:08:51 +0100

    PasteSpell: Don't parse above or below build limit

--- core/src/main/java/com/nisovin/magicspells/spells/targeted/PasteSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/targeted/PasteSpell.java
@@ -508,6 +508,8 @@ public class PasteSpell extends TargetedSpell implements TargetedLocationSpell {
 
 				Block bl = this.target.getBlock().getRelative(pos_.getX() - origin.getX(), pos_.getY() - origin.getY(), pos_.getZ() - origin.getZ());
 
+				if (bl.getY() > bl.getWorld().getMaxHeight() || bl.getY() < bl.getWorld().getMinHeight()) continue;
+				
 				if (this.onlyReplaceAir && !bl.getBlockData().getMaterial().isAir()) continue;
 
 				if (!data.matches(bl.getBlockData())) {
